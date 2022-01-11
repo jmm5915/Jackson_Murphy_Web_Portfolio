06bc55305ac6203c67d2b40e25aee2561887d7fe
jmm5915 <69988863+jmm5915@users.noreply.github.com>
2022-01-11 14:36:20 -0500

    committing test name change

--- animation.js
+++ animation.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
- consoleText(['Jackson Murphy'], 'text',['white']);
+ consoleText(['Quan Lu'], 'text',['white']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
