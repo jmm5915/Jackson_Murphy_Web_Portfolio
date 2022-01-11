79e709b02d4ad51f15227011fcaa67f969d32087
jmm5915 <69988863+jmm5915@users.noreply.github.com>
2022-01-11 14:48:04 -0500

    changed name and colors

--- animation.js
+++ animation.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
- consoleText(['Quan Lu'], 'text',['white']);
+ consoleText(['Jackson Murphy'], 'text',['white']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
