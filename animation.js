75e9a72e0ff2e37dc10db81b44560034a75dd424
jmm5915 <69988863+jmm5915@users.noreply.github.com>
2022-01-11 11:11:28 -0500

    Update animation.js

--- animation.js
+++ animation.js
@@ -48,4 +48,26 @@ function consoleText(words, id, colors) {
       visible = true;
     }
   }, 400)
+
+
 }
+var textWrapper = document.querySelector('projects');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: 'projects .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: 'projects .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
